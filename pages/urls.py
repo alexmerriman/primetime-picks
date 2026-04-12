1f3ccef71602759c1224bab6d85733c053e1e49b
Alex Merriman <alex.merriman2@mail.dcu.ie>
2026-04-12 17:44:47 +0100

    Added basic site navigation

--- pages/urls.py
+++ pages/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('programmes/', views.programme_list, name='programme_list'),
     path('', views.index, name='index'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .models import Programme
 
 def index(request):
     return render(request, 'base.html')
+
+def programme_list(request):
+    programmes = Programme.objects.all()
+    context = {
+        'programmes': programmes
+    }
+    return render(request, 'pages/programme_list.html', context)
