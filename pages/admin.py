62b82736aaad76351929a1d73a746b6bf6e85106
Alex Merriman <alex.merriman2@mail.dcu.ie>
2026-04-15 22:13:06 +0100

    Rebuilt database with Genre model and restored programme data

--- pages/admin.py
+++ pages/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Programme
+from .models import Programme, Genre
 
 admin.site.register(Programme)
+admin.site.register(Genre)
--- pages/models.py
+++ pages/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 class Programme(models.Model):
     title = models.CharField(max_length=200)
-    genre = models.CharField(max_length=100)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
     release_year = models.IntegerField()
     description = models.TextField(blank=True)
     image = models.ImageField(upload_to='programme_images/', blank=True, null=True)
