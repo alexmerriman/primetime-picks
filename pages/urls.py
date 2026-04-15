fb4d6214e23e609f4eab165bd6311055415a28c9
Alex Merriman <alex.merriman2@mail.dcu.ie>
2026-04-16 00:24:33 +0100

    Added CRUD actions to programme list page

--- pages/urls.py
+++ pages/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('programmes/create/', views.ProgrammeCreate.as_view(), name='programme_create'),
+    path('programmes/update/<pk>/', views.ProgrammeUpdate.as_view(), name='programme_update'),
+    path('programmes/delete/<pk>/', views.ProgrammeDelete.as_view(), name='programme_delete'),
     path('programmes/<int:programme_id>/', views.programme_detail, name='programme_detail'),
     path('programmes/', views.programme_list, name='programme_list'),
     path('', views.index, name='index'),
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Programme
 
 def index(request):
@@ -21,3 +23,20 @@ def programme_detail(request, programme_id):
         'programme': programme
     }
     return render(request, 'pages/programme_detail.html', context)
+
+class ProgrammeCreate(CreateView):
+    model = Programme
+    template_name = 'pages/programme_form.html'
+    fields = ['title', 'genre', 'release_year', 'description', 'image']
+    success_url = reverse_lazy('programme_list')
+
+class ProgrammeUpdate(UpdateView):
+    model = Programme
+    template_name = 'pages/programme_form.html'
+    fields = ['title', 'genre', 'release_year', 'description', 'image']
+    success_url = reverse_lazy('programme_list')
+
+class ProgrammeDelete(DeleteView):
+    model = Programme
+    template_name = 'pages/programme_confirm_delete.html'
+    success_url = reverse_lazy('programme_list')
