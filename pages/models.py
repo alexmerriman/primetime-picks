6d887c75e4e93558f2788f54e8bf9bba2dc95830
Alex Merriman <alex.merriman2@mail.dcu.ie>
2026-04-15 19:17:06 +0100

    Added programme image uploads and display on detail page

--- pages/models.py
+++ pages/models.py
@@ -5,6 +5,7 @@ class Programme(models.Model):
     genre = models.CharField(max_length=100)
     release_year = models.IntegerField()
     description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='programme_images/', blank=True, null=True)
 
     def __str__(self):
         return self.title
