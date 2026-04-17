168107d4f5bcc6e0a0e92f8daaead97f3eeeca7a
Alex Merriman <alex.merriman2@mail.dcu.ie>
2026-04-17 16:01:08 +0100

    Improved admin panel with list display ordering and search

--- pages/admin.py
+++ pages/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 from .models import Programme, Genre, Review
 
-admin.site.register(Programme)
-admin.site.register(Genre)
-admin.site.register(Review)
+
+class ProgrammeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'genre', 'release_year')
+    ordering = ('title',)
+    search_fields = ('title', 'genre__name')
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    ordering = ('name',)
+    search_fields = ('name',)
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('programme', 'reviewer_name', 'rating')
+    ordering = ('programme', 'reviewer_name')
+    search_fields = ('programme__title', 'reviewer_name')
+
+
+admin.site.register(Programme, ProgrammeAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Review, ReviewAdmin)
